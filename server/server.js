9256b368173838d1c4cccf4d9bedaf409dd8730d
km4891 <kmorarity@gmail.com>
2021-01-14 20:51:07 -0800

    setting up schema

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// create new Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate Apollo with Express application middleware
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log to test GQL API
+    console.log(`use GraphQL at http://localhost"${PORT}${server.graphqlPath}`);
   });
 });

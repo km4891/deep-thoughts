d21f6224b40954083d1bce8aea8e4029807c7bad
km4891 <kmorarity@gmail.com>
2021-01-19 17:49:42 -0800

    first query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,29 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+
+
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
-  }
+
+type Thought {
+  _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+}
+
+type Reaction {
+  _id: ID
+  reactionBody: String
+  createdAt: String
+  username: String
+}
+
+type Query {
+  thoughts(username: String): [Thought]
+}
 `;
 
 // export the typeDefs
